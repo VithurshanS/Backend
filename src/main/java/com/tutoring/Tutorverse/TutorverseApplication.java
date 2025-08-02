5dd365bfc62f0e80c93f7cd23f9d3fc9a4212406
Tiran Vitharana <vitharanats.22@uom.lk>
2025-08-02 11:53:58 +0530

    Initializing backend

---
+++ src/main/java/com/tutoring/Tutorverse/TutorverseApplication.java
@@ -0,0 +1,13 @@
+package com.tutoring.Tutorverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutoring/Tutorverse/TutorverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoring.Tutorverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

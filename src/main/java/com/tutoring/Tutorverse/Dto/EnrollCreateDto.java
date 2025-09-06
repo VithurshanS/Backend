fc371893e0d29e6a41949819fcc164a57c87d594
Vithurshan Sivanathan <vithurshansivanathan610@gmail.com>
2025-09-06 21:17:53 +0530

    student enrolment is added

---
+++ src/main/java/com/tutoring/Tutorverse/Dto/EnrollCreateDto.java
@@ -0,0 +1,13 @@
+
+package com.tutoring.Tutorverse.Dto;
+import lombok.*;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EnrollCreateDto {
+    private UUID studentId;
+    private UUID moduleId;
+}

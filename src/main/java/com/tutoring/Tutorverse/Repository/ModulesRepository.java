7fb26e0af48234ea29e46f7dc43cebec9105103c
Vithurshan Sivanathan <vithurshansivanathan610@gmail.com>
2025-09-07 00:09:48 +0530

    role based meeting joining function is implemented

--- src/main/java/com/tutoring/Tutorverse/Repository/ModulesRepository.java
+++ src/main/java/com/tutoring/Tutorverse/Repository/ModulesRepository.java
@@ -4,10 +4,12 @@ import java.util.Collection;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.tutoring.Tutorverse.Model.ModuelsEntity;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ModulesRepository extends JpaRepository<ModuelsEntity, UUID> {
 
     Collection<ModuelsEntity> findByNameContainingIgnoreCase(String query);
     Collection<ModuelsEntity> findByDomain_DomainId(Integer domainId);
-
+    Boolean existsByTutorIdAndModuleId(UUID tutorId, UUID moduleId);
 }

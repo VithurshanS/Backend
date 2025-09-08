b8f9c55c53a91c304c71f1c1d068604862ae402a
Tiran Vitharana <vitharanats.22@uom.lk>
2025-09-08 17:53:44 +0530

    Automated emaling done

---
+++ src/main/java/com/tutoring/Tutorverse/Repository/EmailRepository.java
@@ -0,0 +1,19 @@
+package com.tutoring.Tutorverse.Repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tutoring.Tutorverse.Model.EnrollmentEntity;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface EmailRepository extends JpaRepository<EnrollmentEntity, UUID> {
+
+    @Query("SELECT u.email FROM User u " +
+            "JOIN EnrollmentEntity e ON u.id = e.student.user.id " +
+            "WHERE e.module.moduleId = :moduleId")
+    List<String> findEmailsByModuleId(@Param("moduleId") UUID moduleId);
+}

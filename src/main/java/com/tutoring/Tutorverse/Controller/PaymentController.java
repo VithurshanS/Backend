92e46a2051293718c44baf245106a3b69085ccb1
VithurshanS <vithurshansivanathan610@gmail.com>
2025-10-03 16:10:52 +0530

    Fix CORS origin URL in PaymentController

--- src/main/java/com/tutoring/Tutorverse/Controller/PaymentController.java
+++ src/main/java/com/tutoring/Tutorverse/Controller/PaymentController.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-@CrossOrigin(origins ="https://fronttest.shancloudservice.com")
+@CrossOrigin(origins ="https://frontend.shancloudservice.com")
 @RestController
 @RequestMapping("/api/payments")
 public class PaymentController {

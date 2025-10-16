9f3cac4f82cb85787a6e10b841496b561515c4c9
VithurshanS <vithurshansivanathan610@gmail.com>
2025-10-16 22:40:39 +0530

    Fix: Update CORS configuration to include new frontend URL

--- src/main/java/com/tutoring/Tutorverse/Controller/PaymentController.java
+++ src/main/java/com/tutoring/Tutorverse/Controller/PaymentController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 import java.util.UUID;
 
 
-@CrossOrigin(origins ="https://frontend.shancloudservice.com")
+@CrossOrigin(origins ={"https://frontend.shancloudservice.com","https://m.tutorverse.app"})
 @RestController
 @RequestMapping("/api/payments")
 @Slf4j
